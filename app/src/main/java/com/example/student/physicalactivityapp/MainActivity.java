c85406b3072fe5b779b9e542029a497ba0206acd
kb861 <katabie861@student.polsl.pl>
2020-01-03 23:32:47 +0100

    Get text file from storage.

--- app/src/main/java/com/example/student/physicalactivityapp/MainActivity.java
+++ app/src/main/java/com/example/student/physicalactivityapp/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.student.physicalactivityapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static  final  int PERMISSION_REQUEST_STORAGE=1000;
+    private static  final  int READ_REQUEST_CODE=1000;
+
     @Nullable
     @BindView(R.id.btn_next)
     Button btn_next;
+
+    @Nullable
+    @BindView(R.id.btn_addData)
+    Button btn_addData;
 
     @Nullable
     @OnClick(R.id.btn_next)
@@ -25,10 +53,60 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Nullable
+    @OnClick(R.id.btn_addData)
+    void onClickLoadData(View view) {
+      performFileSearch();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+               requestPermissions( new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+
+        }
     }
+    private String readText(String input){
+        File file = new File(input);
+        StringBuilder text = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+             String line;
+        while ((line = br.readLine())!= null) {
+             text.append(line);
+             text.append("\n");
+         }
+        }catch (IOException e){
+            e.printStackTrace();
+    }
+    return text.toString();
+    }
+
+    private void performFileSearch(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent,READ_REQUEST_CODE);
+
+    }
+@Override
+    protected  void  onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+
+            if(data != null){
+                Uri uri = data.getData();
+                String path = uri.getPath();
+                path = path.substring(path.indexOf(":")+1);
+                Toast.makeText(this,""+path,Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+
 }
